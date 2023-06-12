b85fd17392f36c02f760a9757887321ba50fa45f
huhubun <nongzc@outlook.com>
2023-06-13 00:13:17 +0800

    Implement "detail" view mode

--- src/JWTrix.App/Helpers/DefaultJsonSerializerOptions.cs
+++ src/JWTrix.App/Helpers/DefaultJsonSerializerOptions.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 
 namespace JWTrix.App.Helpers
@@ -11,6 +13,7 @@ namespace JWTrix.App.Helpers
     {
         public static JsonSerializerOptions WriteIndented { get; } = new()
         {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
             WriteIndented = true
         };
     }
